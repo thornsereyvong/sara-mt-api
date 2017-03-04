88f1f76b3dfae408b68ae01292d99c969773a37f
sthorn <sthorn@STHORN-PC.balancika.local>
2017-03-04 08:33:00 +0700

    hrfhfhfhf

--- src/main/java/balancika/ame/service/impl/CashAdvanceClearanceServiceImpl.java
+++ src/main/java/balancika/ame/service/impl/CashAdvanceClearanceServiceImpl.java
@@ -1,9 +1,12 @@
 package balancika.ame.service.impl;
 
+import org.springframework.stereotype.Repository;
+
 import balancika.ame.entities.MeDataSource;
 import balancika.ame.entities.tansaction.CashAdvanceClearance;
 import balancika.ame.service.CashAdvanceClearanceService;
 
+@Repository
 public class CashAdvanceClearanceServiceImpl implements CashAdvanceClearanceService{
 
 	@Override
