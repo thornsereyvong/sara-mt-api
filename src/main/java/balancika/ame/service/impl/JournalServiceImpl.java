002dca9e5df574e62c2576093720dc4250c3fde0
sthorn <sthorn@STHORN-PC.balancika.local>
2017-03-04 08:31:32 +0700

    egegeheheh

--- src/main/java/balancika/ame/service/impl/JournalServiceImpl.java
+++ src/main/java/balancika/ame/service/impl/JournalServiceImpl.java
@@ -1,9 +1,12 @@
 package balancika.ame.service.impl;
 
+import org.springframework.stereotype.Repository;
+
 import balancika.ame.entities.MeDataSource;
 import balancika.ame.entities.tansaction.Journal;
 import balancika.ame.service.JournalService;
 
+@Repository
 public class JournalServiceImpl implements JournalService{
 
 	@Override
